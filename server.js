8be46ef742602636e4205f44d8cc60f188edef66
wallacce135 wallacce135 <wallacce135@gmail.com>
2022-05-22 14:53:05 +0300

    Initialization server commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+
+const pool = mysql.createPool({
+    connectionLimit: 5,
+    host: "localhost",
+    user: "wallacce135",
+    password: "Olof0re1351",
+    database: "profresouce"
+});
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) =>{
+    pool.query('SELECT * FROM articles', function(err, data){
+        if(err) return console.log(err);
+
+        res.send(data);
+    })
+});
+
+app.post("/newUser", (req, res)=>{
+    // console.log(req);
+    pool.query(`INSERT INTO Users (user_login, user_password, user_email) VALUES (?,?,?)`, [req.body.user.Login, req.body.user.Password, req.body.user.Email], function(err, data){
+        if(err) return console.log(err);
+
+        res.redirect('/');
+    })
+})
+
+app.post("/login", (req, res) =>{
+    pool.query(`SELECT * FROM Users WHERE user_login=? AND user_password=?`, [req.body.login, req.body.password], function (err, data){
+        if(err) return console.log(err);
+        res.send(data);
+    })
+})
+
+app.get("/tips", (req, res)=>{
+    pool.query('SELECT * FROM tips', function(err, tips){
+        if(err) return console.log(err);
+        res.send(tips);
+    })
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.post("/uploadNewArticle", (req, res)=>{
+    pool.query(`INSERT INTO Articles(article_title, article_text, user_id) VALUES (?, ?, ?)`, [req.body.payload.payload.artTitle, req.body.payload.payload.artText, req.body.payload.payload.user_id], function(err, data){
+        if(err) return console.log(err)
+        res.redirect("/");
+    });
+})
+
+app.post('/getArticle', (req, res)=>{
+    console.log(req.body);
+    pool.query(`SELECT * FROM Articles WHERE (article_id) = ?`, [req.body.article_id], function(err, data){
+        if(err) return console.log(err)
+        res.send(data);
+    }) 
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port: ${PORT}`)
+});
+
+
+
+
+module.exports = app;
